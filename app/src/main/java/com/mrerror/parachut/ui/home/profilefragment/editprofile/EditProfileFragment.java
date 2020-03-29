26d9db225f6cb49e793f8e0b99a4c33418cd2f1b
Mohamed Hani <mhani642@gmail.com>
2020-03-29 18:53:49 +0200

    editProfile MH

--- app/src/main/java/com/mrerror/parachut/ui/home/profilefragment/editprofile/EditProfileFragment.java
+++ app/src/main/java/com/mrerror/parachut/ui/home/profilefragment/editprofile/EditProfileFragment.java
@@ -10,6 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.mrerror.parachut.Models.GetUserData;
@@ -17,6 +18,7 @@ import com.mrerror.parachut.R;
 import com.mrerror.parachut.databinding.EditProfileFragmentBinding;
 import com.mrerror.parachut.ui.home.profilefragment.ProfileFragment;
 import com.mrerror.parachut.utils.GlobalPrefrencies;
+import com.mrerror.parachut.utils.Utils;
 
 public class EditProfileFragment extends Fragment {
 
@@ -31,10 +33,10 @@ public class EditProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         editProfileFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.edit_profile_fragment, container, false);
+        Utils.setLocale(getContext());
         return editProfileFragmentBinding.getRoot();
     }
     GlobalPrefrencies globalPrefrencies;
-    GetUserData model;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -48,25 +50,40 @@ public class EditProfileFragment extends Fragment {
         editProfileFragmentBinding.save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                GetUserData userModel = mViewModel.onClickUpdate(
-                        editProfileFragmentBinding.nameId.getText().toString().trim(),
-                        editProfileFragmentBinding.addressId.getText().toString().trim(),
-                        editProfileFragmentBinding.phoneId.getText().toString(),
-                        editProfileFragmentBinding.passId.getText().toString().trim(),
-                        "",
-                        "30.000","33.000"
-
-                        ,getContext());
-
+                Update();
                 showFragment(new ProfileFragment());
+            }
+        });
+    }
+
+    private void Update() {
+
+        mViewModel.onClickUpdate(
+                editProfileFragmentBinding.nameId.getText().toString().trim(),
+                editProfileFragmentBinding.addressId.getText().toString().trim(),
+                editProfileFragmentBinding.phoneId.getText().toString(),
+                editProfileFragmentBinding.passId.getText().toString().trim(),
+                "",
+                "30.000","33.000",
+                getContext()
+        );
+
+        mViewModel.userDataMutableLiveData.observe(getViewLifecycleOwner(), new Observer<GetUserData>() {
+            @Override
+            public void onChanged(GetUserData getUserData) {
+
+                editProfileFragmentBinding.nameId.setText(getUserData.getName()+"");
+                editProfileFragmentBinding.phoneId.setText(getUserData.getMobile() + "");
+                editProfileFragmentBinding.addressId.setText(getUserData.getAddress() + "");
+
             }
         });
 
 
     }
     private void showFragment(Fragment fragment) {
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        ft.replace(R.id.container, fragment);
+        FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+        ft.replace(R.id.containerprofile, fragment);
         ft.addToBackStack(null);
         ft.commit();
     }
